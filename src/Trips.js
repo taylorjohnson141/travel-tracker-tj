c8df8afff06454c5da56fbf3521424342341f077
Taylor Johnson <taylorjohnson141@gmail.com>
2020-09-19 15:55:28 -0600

    Add findUserTrips mthod to Trips.js

--- src/Trips.js
+++ src/Trips.js
@@ -1,6 +1,14 @@
 class Trips {
   constructor(id,travelData,destinationData) {
     this.currentUserId = id;
+    this.travelData = travelData
+    this.destinationData = destinationData
     this.currentUserTrips;
   }
+  findUserTrips() {
+    this.currentUserTrips = this.travelData.filter(trip =>{
+      return trip.userID === this.currentUserId
+    })
+  }
+
 }

8e3227d5b3478ce59491a2c5c331e3df662b867d
Taylor Johnson <taylorjohnson141@gmail.com>
2020-09-19 14:43:18 -0600

    Add test for should be able to find the currect User Trips correctly

--- test/trip-test.js
+++ test/trip-test.js
@@ -7,8 +7,12 @@ describe('Trip', () => {
   beforeEach(() => {
     trips = new Trips(id, TripData, destinationData);
   });
-  it('should contain an Id',() =>{
+  it('should contain an id',() =>{
     expect(trips.id).to.equal(1)
   })
+  it('should be able to find the currect User Trips correctly',() =>{
+    trips.findUserTrips()
+    expect(trips.currentUserTrips).to.deep.equal()
+  }
   
 })

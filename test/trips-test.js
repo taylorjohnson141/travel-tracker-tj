66f2d3e27405cd4e48946a0a0054590d9d3aae9d
Taylor Johnson <taylorjohnson141@gmail.com>
2020-09-19 16:07:20 -0600

    Refactor test data to support tests

--- test/trips-test.js
+++ test/trips-test.js
@@ -1,16 +1,16 @@
 import chai from 'chai';
-import Trips from '../src/Tripsa.js'
+import Trips from '../src/Trips.js'
 const expect = chai.expect;
 let id = 1
 let TripData = [
-{"id":1,"userID":44,"destinationID":49,"travelers":1,"date":"2019/09/16","duration":8,"status":"approved","suggestedActivities":[]}, 
+{"id":1,"userID":1,"destinationID":1,"travelers":1,"date":"2019/09/16","duration":8,"status":"approved","suggestedActivities":[]}, 
 {"id":2,"userID":35,"destinationID":25,"travelers":5,"date":"2020/10/04","duration":18,"status":"pending","suggestedActivities":[]},
 {"id":3,"userID":3,"destinationID":22,"travelers":4,"date":"2020/05/22","duration":17,"status":"pending","suggestedActivities":[]},
-{"id":4,"userID":43,"destinationID":14,"travelers":2,"date":"2020/02/25","duration":10,"status":"approved","suggestedActivities":[]},
+{"id":4,"userID":1,"destinationID":2,"travelers":2,"date":"2020/02/25","duration":10,"status":"approved","suggestedActivities":[]},
 {"id":5,"userID":42,"destinationID":29,"travelers":3,"date":"2020/04/30","duration":18,"status":"approved","suggestedActivities":[]},
-{"id":6,"userID":29,"destinationID":35,"travelers":3,"date":"2020/06/29","duration":9,"status":"approved","suggestedActivities":[]},
+{"id":6,"userID":1,"destinationID":4,"travelers":3,"date":"2020/06/29","duration":9,"status":"approved","suggestedActivities":[]},
 {"id":7,"userID":37,"destinationID":17,"travelers":5,"date":"2020/5/28","duration":20,"status":"approved","suggestedActivities":[]},
-{"id":8,"userID":36,"destinationID":39,"travelers":6,"date":"2021/02/07","duration":4,"status":"approved","suggestedActivities":[]}
+{"id":8,"userID":36,"destinationID":5,"travelers":6,"date":"2021/02/07","duration":4,"status":"approved","suggestedActivities":[]}
 ];
 
 let destinationData = [
@@ -31,7 +31,7 @@ describe('Trip', () => {
     trips = new Trips(id, TripData, destinationData);
   });
   it('should contain an id',() =>{
-    expect(trips.id).to.equal(1)
+    expect(trips.currentUserId).to.equal(1)
   })
   it('should be able to find the currect User Trips correctly',() =>{
     trips.findUserTrips()

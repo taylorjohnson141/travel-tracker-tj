e4739b2b996dd01d78089aac9e5d1f1070b53cee
Taylor Johnson <taylorjohnson141@gmail.com>
2020-09-19 19:38:25 -0600

    Add getData method on fetchRequest class

--- src/fetchRequests.js
+++ src/fetchRequests.js
@@ -2,8 +2,18 @@ class FetchRequests {
   constructor(id) {
     this.currentUserId = id
     this.currentUserData;
-    this.travelData;
+    this.tripData;
     this.DestinationData;
   }
-  
+  getData() {
+    Promise.all([
+      `https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/travelers/travelers/${this.currentUserId}`,
+      `https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/trips/trips`,
+      `https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/destinations/destinations`
+    ]).then(data =>{
+      this.currentUserData = data[0]
+      this.tripData = data[1]
+      this.DestinationData = data[3]
+    })
+  }
 }

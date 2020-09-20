2f26913280073091742c7b9b6e854f0d225b171b
Taylor Johnson <taylorjohnson141@gmail.com>
2020-09-20 17:38:19 -0600

    Add addDestinations function to dom updates

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,3 +1,18 @@
-domUpdates = {
-  
+let userTravelSection = document.querySelector('.user-travels')
+let domUpdates = {
+
+  addDestinations(userTravels) {
+    userTravelSection.innerHTML = ''
+    userTravels.forEach(trip => {
+      userTravelSection.innerHTML +=
+      `<article id = ${trip.id} class="single-recipe-card ${trip.status}">
+         <article class="card-image-section">
+           <img class="card-image" tabindex="0" src="${trip.image}" alt= ${trip.destination.alt}>
+         </article>
+         <article class="recipe-name-area">
+           <h2 class="recipe-name" tabindex="0">${trip.destination.name}</h2>
+         </article>
+       </article>`
+    });
+  }
 }

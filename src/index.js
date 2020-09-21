0b20d11b714597af4b32f50788dcb153112e5c6f
Taylor Johnson <taylorjohnson141@gmail.com>
2020-09-20 18:02:48 -0600

    Add initial imports

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
+import domUpdates from './domUpdates'
+import FetchRequests from './fetchRequests'
 
+window.onload = () =>{
+
+}
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
 
-console.log('This is the JavaScript entry file - your code begins here.');

d707ca279219ab341e44bcce9620dffb77cd7fa5
Taylor Johnson <taylorjohnson141@gmail.com>
2020-09-21 09:53:47 -0600

    Add findStatus method to Trips.js

--- src/Trips.js
+++ src/Trips.js
@@ -1,3 +1,5 @@
+import moment from 'moment'
+
 class Trips {
   constructor(id, travelData, destinationData) {
     this.currentUserId = id;
@@ -9,6 +11,8 @@ class Trips {
     this.currentUserTrips = this.travelData.filter(trip =>{
       return trip.userID === this.currentUserId
     })
+    this.formatTripsAndDestination()
+    this.findStatus()
   }
   formatTripsAndDestination() {
     this.currentUserTrips.forEach(trip => { 
@@ -17,6 +21,25 @@ class Trips {
       })
       trip.destination = correctDestination
     })
+  }
+  findStatus () {
+    let today = new Date()
+    this.currentUserTrips.forEach(trip =>{
+      let array = trip.date.split('/')
+      let correctDate = array.join('-')
+      let tripDay = moment(correctDate);
+      console.log('day', tripDay)
+      if (trip.status === 'pending' &&  !(tripDay < today)) {
+        trip.status = 'pending'
+      } else if (tripDay > today ) {
+        trip.status = 'future'
+      } else if (tripDay.add(trip.duration, 'days').toDate() < today) {
+        trip.status = 'past'
+      } else {
+        trip.status = 'current trip'
+      }
+    })
+
   }
   // findCost(trip) {
   //   trip.

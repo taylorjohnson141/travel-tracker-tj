11a35739cdc875598eef926476fccb9e8c2d9969
Taylor Johnson <taylorjohnson141@gmail.com>
2020-09-21 16:21:17 -0600

    Add findID method to Trip.js

--- src/Trip.js
+++ src/Trip.js
@@ -1,8 +1,9 @@
 
 class Trip {
-  constructor(currentUserId,id dataForTrip, destinationData) {
+  constructor(currentUserId,id dataForTrip,tripData, destinationData) {
     this.currentUserId = currentUserId
-    this.id;
+    this.id= this.findID()
+    this.tripData;
     this.dataForTrip = dataForTrip;
     this.destinationData = destinationData;
     this.estimatedCost;
@@ -15,5 +16,23 @@ class Trip {
     this.estimatedCost += (curr.travelers * curr.destination.estimatedFlightCostPerPerson)
     return this.estimatedCost
   }
-   
+   findID (){
+    return tripData.reduce((acc,trip) => {
+      if(acc === trip.id){
+        acc += this.findUniqueValue(trip.id,acc)
+      }
+      else{
+        return acc
+      }
+    },0)
+   }
+   findUniqueValue(trip,value){
+    value +=1
+     if(trip === value){
+      return this.findUniqueValue(trip,value)
+     }
+     else{
+       return value
+     }
+   }
 }

1c816dc1c08db1d38af72225db574fcbc9341d9d
Taylor Johnson <taylorjohnson141@gmail.com>
2020-09-21 16:00:51 -0600

    Add calculateEstimatedCost function

--- src/Trip.js
+++ src/Trip.js
@@ -1,11 +1,19 @@
 
 class Trip {
-  constructor(id, dataForTrip, destinationData) {
-    this.id = id;
+  constructor(currentUserId,id dataForTrip, destinationData) {
+    this.currentUserId = currentUserId
+    this.id;
     this.dataForTrip = dataForTrip;
     this.destinationData = destinationData;
     this.estimatedCost;
   }
-
+  calculateEstimatedCost(){
+    currectDestination = destinationData.find(destination =>{
+      this.destinationData.name === destination.destination
+    })
+    this.estimatedCost += curr.destination.estimatedLodgingCostPerDay
+    this.estimatedCost += (curr.travelers * curr.destination.estimatedFlightCostPerPerson)
+    return this.estimatedCost
+  }
    
 }
